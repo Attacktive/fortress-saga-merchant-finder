c37fcf727088cf1628287c62c443ba20ee9bb948
Attacktive <tribute2pantera@gmail.com>
2025-12-24 10:38:54 +0900

    refactor: yeet manual `Promise` wiring

--- src/discord.ts
+++ src/discord.ts
@@ -1,66 +1,47 @@
+import { once } from 'node:events';
 import { Client } from 'discord.js';
 import { getLastCoordinates } from './coordinates.js';
 
 export const useDiscord = async () => {
-	const TOKEN = process.env.TOKEN;
-	if (!TOKEN) {
-		throw new Error('The environment variable "TOKEN" is not found in environment variables!');
-	} else if (TOKEN.includes('...')) {
+	const token = process.env.TOKEN;
+	const channelId = process.env.CHANNEL_ID;
+
+	if (!token || token.includes('...')) {
 		throw new Error('You need to provide the environment variable "TOKEN"!');
 	}
 
-	const CHANNEL_ID = process.env.CHANNEL_ID;
-	if (!CHANNEL_ID) {
-		throw new Error('The environment variable "CHANNEL_ID" is not found in environment variables!');
-	} else if (CHANNEL_ID.includes('...')) {
+	if (!channelId || channelId.includes('...')) {
 		throw new Error('You need to provide the environment variable "CHANNEL_ID"!');
 	}
 
-	let succeed: () => void;
-	let fail: (_error: unknown) => void;
-	const finished: Promise<void> = new Promise((resolve, reject) => {
-		succeed = resolve;
-		fail = reject;
-	});
-
 	const client = new Client({ intents: [] });
 
-	client.once(
-		'clientReady',
-		async () => {
-			const { user } = client;
+	try {
+		await client.login(token);
+		await once(client, 'clientReady');
 
-			if (user) {
-				console.log(`🤖 Discord bot logged in as ${user?.tag}!`);
-			} else {
-				console.log('🤖 Discord bot logged in.');
-			}
-
-			try {
-				const coordinates = getLastCoordinates();
-				const channel = await client.channels.fetch(CHANNEL_ID);
-				if (channel) {
-					if (channel.isSendable()) {
-						await channel.send(`\`${coordinates}\``);
-					} else {
-						fail(new Error(`Channel is not sendable: ${CHANNEL_ID}`));
-					}
-				} else {
-					fail(new Error(`Channel not found: ${CHANNEL_ID}`));
-				}
-			} catch (error) {
-				fail(error);
-			} finally {
-				await client.destroy();
-
-				succeed();
-			}
+		const { user } = client;
+		if (user) {
+			console.log(`🤖 Discord bot logged in as ${user.tag}!`);
+		} else {
+			console.log('🤖 Discord bot logged in.');
 		}
-	);
 
-	await client.login(TOKEN);
+		const coordinates = getLastCoordinates();
+		const channel = await client.channels.fetch(channelId);
 
-	await finished;
+		if (!channel) {
+			throw new Error(`Channel not found: ${channelId}`);
+		}
+
+		if (!channel.isSendable()) {
+			throw new Error(`Channel is not sendable: ${channelId}`);
+		}
+
+		await channel.send(`\`${coordinates}\``);
+	} finally {
+		await client.destroy();
+	}
 
 	return client;
 };
